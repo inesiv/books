71e28330f6d6e9b03adbaa54cfd611c32b4ba1d6
Ines Ivask <ines.ivask@khk.ee>
2021-12-02 17:46:16 +0200

    Delete single book from books list

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const form = document.querySelector('form');
 const taskInput = document.querySelector('#task');
+const tasksList = document.querySelector('.collection');
 
-form.addEventListener('submit', Submit)
+form.addEventListener('submit', Submit);
+tasksList.addEventListener('click', deleteTask);
+
+
+function deleteTask(event){
+    if(event.target.textContent == 'X');{
+        if(confirm("Do you want to delete this book?")){
+            (event.target.parentElement.remove());
+        }
+    }
+}
+
 
 function Submit(event){
     // input value
@@ -14,6 +26,18 @@ function Submit(event){
     const text = document.createTextNode(task);
     // add text value to <li>
     li.appendChild(text);
+    // create link element
+    const link = document.createElement('a');
+    // set href attribute
+    link.setAttribute('href', '#');
+    // add CSS style
+    link.className = 'secondary-content';
+    // add X text to link
+    link.appendChild(document.createTextNode('X'));
+    // add link to <li>
+    li.appendChild(link);
+
+
     // find <ul> DOM component
     const ul = document.querySelector('.collection');
     // add <li> to <ul>
